6b29da7da22bc0760a39839398e036b66397dc00
Tisha Patel <81203261+tishapatel@users.noreply.github.com>
2021-05-11 00:04:25 -0400

    Update BenfordsLaw.java

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -2,7 +2,7 @@
  * Date: May 6th 2021
  * Name: Sasha and Tisha
  * Teacher: Mr.Ho
- * Description: Part 2 of the methods assignment using Benford's Law and a Sales cvs file
+ * Description: Part 2 of the methods assignment using Benford's Law and a Sales/Results cvs file.
 */
 
 
